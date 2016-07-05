8dd54997123ce7dcb4aa646d66dbb3ff219a123a
Pupinder Pathania <pathaniarajput92@gmail.com>
2016-07-06 01:59:51 +0530

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -4,7 +4,7 @@
 	use phpish\shopify;
 	require __DIR__.'/conf.php';
 	# Guard: http://docs.shopify.com/api/authentication/oauth#verification
-	shopify\is_valid_request_hmac($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
+	shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
 	
 	# Step 2: http://docs.shopify.com/api/authentication/oauth#asking-for-permission
 	if (!isset($_GET['code']))
